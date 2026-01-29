d1615e6f9f8885155f4387bdf9233805fe956d8a
zirkoh0404-coder <zirkoh0404@gmail.com>
2026-01-29 16:02:29 +0530

    Update patchright.js

--- patchright.js
+++ patchright.js
@@ -1,22 +1,14 @@
 function detect_patchright() {
   try {
-    if (typeof window === 'undefined' || typeof navigator === 'undefined') return false;
-
-    if (window.__pwInitScripts || window.__patchright__ || window.patchright_injected) return true;
-
-    var _els = document.querySelectorAll('script, div, span');
-    for (var i = 0; i < _els.length; i++) {
-        var _id = _els[i].id || '';
-        if (_id.indexOf('__pw_') === 0) return true;
+    var _w = window;
+    var keys = Object.getOwnPropertyNames(_w);
+    for (var i = 0; i < keys.length; i++) {
+      if (keys[i].startsWith('__pw_') || keys[i].startsWith('__playwright_')) return true;
     }
-
-    if (navigator.plugins.toString().indexOf('[native code]') === -1) return true;
-    if (typeof window.Buffer !== 'undefined' || typeof window.process !== 'undefined') return true;
-
-    var _pd = Object.getOwnPropertyDescriptor(navigator, 'webdriver');
-    if (_pd && !_pd.get && _pd.value === false) return true;
-
+    
+    var _e = new Error();
+    if (_e.stack && (_e.stack.includes('__pw') || _e.stack.includes('patchright'))) return true;
   } catch (e) {}
   return false;
 }
-if (typeof window !== 'undefined') window.detect_patchright = detect_patchright;
+window.detect_patchright = detect_patchright;
