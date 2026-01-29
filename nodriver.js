73af63fa6aec39abcafc326c1d7c2214a5636560
zirkoh0404-coder <zirkoh0404@gmail.com>
2026-01-29 15:34:24 +0530

    Update nodriver.js

--- nodriver.js
+++ nodriver.js
@@ -2,23 +2,36 @@ function detect_nodriver() {
   try {
     if (typeof window === 'undefined' || typeof navigator === 'undefined') return false;
 
-    var _d = Object.getOwnPropertyDescriptor(navigator, 'webdriver');
-    if (_d && (_d.get || _d.set)) return true;
+    var _w = window;
+    var _n = navigator;
+
+    var _d = Object.getOwnPropertyDescriptor(_n, 'webdriver');
+    if (_d && _d.get) {
+        var _s = _d.get.toString();
+        if (_s.indexOf('native code') === -1 || _s.length > 35) return true;
+    }
+
+    if (_w.chrome && _w.chrome.runtime) {
+        if (!_w.chrome.app && !_w.chrome.csi) return true;
+    }
+
+    if (_w.nodriver_internal_function !== undefined || _w.__nodriver !== undefined) return true;
+
+    if (_n.constructor.name !== 'Navigator') return true;
 
     var _e = new Error();
     if (_e.stack) {
-        var _l = _e.stack.split('\n');
-        if (_l.length > 13 || _e.stack.indexOf('chrome-extension://') !== -1) return true;
+        var _st = _e.stack;
+        if (_st.indexOf('chrome-extension://') !== -1 || _st.indexOf('nodriver') !== -1) return true;
+        var _l = _st.split('\n');
+        if (_l.length > 13) return true;
     }
 
-    var _p = Object.getOwnPropertyNames(window);
+    var _p = Object.getOwnPropertyNames(_w);
     for (var i = 0; i < _p.length; i++) {
-        var k = _p[i];
-        if (k.indexOf('__nodriver') !== -1 || k.indexOf('_nd_') === 0) return true;
+        var _k = _p[i];
+        if (_k.indexOf('__nodriver') !== -1 || _k.indexOf('_nd_') === 0) return true;
     }
-
-    if (Function.prototype.toString.toString().indexOf('[native code]') === -1) return true;
-    if (window.chrome && window.chrome.runtime && !window.chrome.app) return true;
 
   } catch (e) {}
   return false;
