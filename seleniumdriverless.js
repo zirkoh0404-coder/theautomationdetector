66a911a6d4a708ca663b9938fefe6acc027fed40
zirkoh0404-coder <zirkoh0404@gmail.com>
2026-01-29 15:36:25 +0530

    Update seleniumdriverless.js

--- seleniumdriverless.js
+++ seleniumdriverless.js
@@ -1,30 +1,33 @@
-function detect_seleniumdriverless() {
+function detect_selenium_driverless() {
   try {
-    if (typeof window === 'undefined' || typeof navigator === 'undefined') return false;
-
-    var _d = document;
+    if (typeof window === 'undefined') return false;
     var _w = window;
-    
-    for (var k in _d) {
-      if (k.indexOf('cdc_') > -1 || k.indexOf('$cdc_') > -1) return true;
+    var _n = navigator;
+
+    if (_w.selenium_driverless_context_manager || _w.__selenium_driverless__ || _w.cdc_adoQpoasnfa76pfcZLmcfl_Array) return true;
+
+    if (_n.webdriver && !/Headless/.test(_n.userAgent)) {
+        var _d = Object.getOwnPropertyDescriptor(_n, 'webdriver');
+        if (_d && (_d.value === true || _d.writable === true)) return true;
     }
 
-    if (_w.selenium_driverless_context_manager || _w.__selenium_driverless__) return true;
+    if (_n.constructor.name !== 'Navigator') return true;
 
-    var _desc = Object.getOwnPropertyDescriptor(navigator, 'webdriver');
-    if (_desc && _desc.get) {
-      var _s = _desc.get.toString();
-      if (_s.indexOf('native code') === -1 || _s.length > 35) return true;
+    if (_w.chrome && _w.chrome.runtime && !_w.chrome.csi) return true;
+
+    try {
+        null.f();
+    } catch (e) {
+        if (e.stack && (e.stack.indexOf('selenium') !== -1 || e.stack.indexOf('driverless') !== -1)) return true;
     }
 
-    if (_w.__selenium_evaluate || _w.__selenium_unwrapped) return true;
-
-    var _o = Object.getOwnPropertyNames(_w);
-    for (var i = 0; i < _o.length; i++) {
-      if (_o[i].indexOf('driverless') > -1 && _o[i].indexOf('_') === 0) return true;
+    var _p = Object.getOwnPropertyNames(_w);
+    for (var i = 0; i < _p.length; i++) {
+        var _k = _p[i].toLowerCase();
+        if (_k.indexOf('selenium') !== -1 || _k.indexOf('driverless') !== -1) return true;
     }
 
   } catch (e) {}
   return false;
 }
-if (typeof window !== 'undefined') window.detect_seleniumdriverless = detect_seleniumdriverless;
+if (typeof window !== 'undefined') window.detect_selenium_driverless = detect_selenium_driverless;
